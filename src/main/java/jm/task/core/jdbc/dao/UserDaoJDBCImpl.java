33cc5223d30312d2720398ca346cc77f162fb9e1
ignatiev-free <ignatiev.stat@yandex.ru>
2023-11-10 17:51:25 +0300

    First commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +13,76 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        String request = "CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastName VARCHAR(255), age INT);";
+        try (Connection connection = Util.getConnection();
+                PreparedStatement prpStmnt = connection.prepareStatement(request)) {
+            prpStmnt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        String request = "drop table Users";
+        try (Connection connection = Util.getConnection();
+                PreparedStatement prpStmnt = connection.prepareStatement(request)) {
+            prpStmnt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String request = "insert into Users (name, lastName, age) values (?, ?, ?)";
+        try (Connection connection = Util.getConnection();
+                PreparedStatement prpStmnt = connection.prepareStatement(request)) {
+            prpStmnt.setString(1, name);
+            prpStmnt.setString(2, lastName);
+            prpStmnt.setByte(3, age);
+            prpStmnt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        String request = "delete from Users where id = ?";
+        try (Connection connection = Util.getConnection();
+                PreparedStatement prpStmnt = connection.prepareStatement(request)) {
+            prpStmnt.setLong(1, id);
+            prpStmnt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String request = "select * from users";
+        try (Connection connection = Util.getConnection();
+                    PreparedStatement prpstmnt = connection.prepareStatement(request)) {
+            ResultSet resultSet = prpstmnt.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(Long.valueOf(resultSet.getString("id")));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(Byte.valueOf(resultSet.getString("age")));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        String request = "delete from Users";
+        try (Connection connection = Util.getConnection();
+                PreparedStatement prpStmnt = connection.prepareStatement(request)) {
+            prpStmnt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    public static Connection getConnection() {
+
+        String dbUrl = "jdbc:mysql://localhost:3306/prep";
+        String user = "root";
+        String password = "859929sql";
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(dbUrl, user, password);
+            System.out.println("Подключение успешно");
+        } catch (SQLException e) {
+            System.out.println("Подключение не успешно");
+        }
+        return connection;
+    }
+
 }
